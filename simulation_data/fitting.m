3fd505bc262de007f7682e1068ee325afbe52254
PhonNoimann <robertof.gentile@libero.it>
2020-08-28 10:24:08 +0200

    Added results folder

---
+++ simulation_data/fitting.m
@@ -0,0 +1,33 @@
+t = 1:length(reward); t = t';
+
+pr = polyfit(t,reward,1);
+fr = polyval(pr,t);
+
+pl = polyfit(t,len,1);
+fl = polyval(pl,t);
+
+if pr(1) > 0
+    disp("The robot is learning")
+else
+    disp("The robot is not learning")
+end
+
+plot(t,reward,t,fr,'--',t,len,t,fl,'--'), grid on, title(['\alpha = 0.1, \gamma = 0.9, m = ',num2str(pr(1))])
+
+m = mean(reward);
+outlier = m*3;
+I = find(reward > outlier);
+if length(I) > 0
+    reward(I) = [];
+    len(I) = [];
+    t = 1:length(reward); t = t';
+
+    pr = polyfit(t,reward,1);
+    fr = polyval(pr,t);
+
+    pl = polyfit(t,len,1);
+    fl = polyval(pl,t);
+    
+    figure()
+    plot(t,reward,t,fr,'--',t,len,t,fl,'--'), grid on, title(['Without outliers, m = ', num2str(pr(1))])
+end
